a553dd839f17bba402400c93f2909acb0b10e15d
Dracgamer5643 <supermee5643@gmail.com>
2024-10-09 19:30:27 +0530

    Startedthe skills Page

--- static/js/home.js
+++ static/js/home.js
@@ -3,12 +3,13 @@ const game_con = document.getElementById("game");
 const arr = [1, 1, 2, 2, 3, 3];
 const shuffleArr = shuffleArray(arr);
 
-for(let i = 1; i<=6; i++){
+for(let i = 0; i<6; i++){
 let card = document.createElement("div");
-card_ids = card.id = i;
+card.id = i;
+card.className = "cards";
+card.textContent = shuffleArr[i];
 
-card_val = card.value = shuffleArr[i];
-console.log(card_ids, card_val);
+game_con.appendChild(card);
 }
 
 
@@ -18,9 +19,4 @@ function shuffleArray(array) {
         [array[i], array[j]] = [array[j], array[i]];
     }
     return array;
-}
-
-
-function getRandomInt(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
